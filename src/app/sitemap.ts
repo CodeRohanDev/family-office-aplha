4eb96b9519740d2e49e5de3dedb90dc684766a21
Rohan Kumar <rohankumarinsta@gmail.com>
2025-09-25 22:34:50 +0530

    Revert "Minor"

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,5 @@
 import { MetadataRoute } from 'next'
 
-// Force static generation for sitemap when using "output: export"
-export const dynamic = "force-static";
-
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://familyofficealpha.io'
   const currentDate = new Date().toISOString()
