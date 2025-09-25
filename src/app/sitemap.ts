6653c84c17f0f010cac660dcf1208560ea65721b
Chirag Mittal <chirag.mittal@exampathfinder.org>
2025-09-25 22:30:17 +0530

    Minor

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next'
 
+// Force static generation for sitemap when using "output: export"
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://familyofficealpha.io'
   const currentDate = new Date().toISOString()
