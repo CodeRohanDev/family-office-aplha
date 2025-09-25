080b9837fa5c27e09e035a227cca92165cccb3bd
Rohan Kumar <rohankumarinsta@gmail.com>
2025-09-25 22:38:09 +0530

    Revert "Minor" (#2)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,8 +1,5 @@
 import { MetadataRoute } from 'next'
 
-// Force static generation for robots when using "output: export"
-export const dynamic = "force-static";
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
